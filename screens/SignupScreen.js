15ac6c326e10f51c2888c8dea748fc52d481cd3c
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-11 23:37:10 +0530

    Update add screens\SignupScreen.js

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -11,7 +11,7 @@ const SignUpScreen = ({ navigation }) => {
     // Add logic for signup (API call)
     if (name && email && password) {
       alert('Sign-up successful!');
-      navigation.navigate('Login'); // Navigate to Login after successful sign-up
+      navigation.navigate('Login'); // Navigate to Login after successful sign-ups
     } else {
       setErrorMessage('All fields are required');
     }
