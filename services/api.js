965a3cda5b17ef400c58d0cfc4c42d36ca23a3ea
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-27 00:24:24 +0530

    Update add services\api.js

--- services/api.js
+++ services/api.js
@@ -127,7 +127,7 @@ export const fetchTopRatedMovies = async () => {
 
 // services/api.js
 export const fetchMovieDetails = (movieId) => {
-  // You can replace this with an actual API cal
+  // You can replace this with an actual API ca
   return fetch(`https://api.example.com/movies/${movieId}`)
     .then(response => response.json())
     .catch(error => console.error('Error fetching movie details:', error));
